2f7260062b7410bc3e719d5d5c188a0c4668cc22
Braden Everson <bradeneverson@gmail.com>
2024-04-23 05:09:07 -0500

    Working sun modeling

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::error::Error;
+use std::{error::Error, ops::{Range, RangeBounds}, time::Instant};
 
-use esp_idf_svc::hal::{adc::{attenuation, config::Config, AdcChannelDriver, AdcDriver}, delay::FreeRtos, peripherals::Peripherals};
+use esp_idf_hal::{ledc::{config::TimerConfig, LedcDriver, LedcTimerDriver, Resolution}, units::Hertz};
+use esp_idf_svc::hal::{adc::{attenuation, config::Config, AdcChannelDriver, AdcDriver}, delay::FreeRtos, gpio::PinDriver, peripherals::Peripherals};
 use esp_idf_unda::network::{activations::Activations, network::Network};
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -17,6 +18,32 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut water_sensor: AdcChannelDriver<{attenuation::DB_11}, _> = AdcChannelDriver::new(peripherals.pins.gpio4)?;
     let mut light_sensor: AdcChannelDriver<{attenuation::DB_11}, _> = AdcChannelDriver::new(peripherals.pins.gpio5)?;
 
+    let mut relay = PinDriver::output(peripherals.pins.gpio17)?;
+
+    relay.set_high()?;
+
+    let timer_driver = LedcTimerDriver::new(
+        peripherals.ledc.timer0, 
+        &TimerConfig::default()
+            .frequency(Hertz::from(50))
+            .resolution(Resolution::Bits14))?;
+        
+    let mut driver = LedcDriver::new(
+            peripherals.ledc.channel0, timer_driver, peripherals.pins.gpio11)?;
+
+    let max_duty = driver.get_max_duty();
+    let min_limit = max_duty * 25 / 1000;
+    let max_limit = max_duty * 125 / 1000;
+
+
+
+    driver.set_duty(map(0, 0, 180, min_limit + 250, max_limit))?;
+
+    FreeRtos::delay_ms(2000);
+
+    //move_to(&mut driver, 30..200, min_limit, max_limit)?;
+    
+
 
     let sun_model_str = "";
     let water_model_str = "";
@@ -26,10 +53,12 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let mut days_since = 0f32;
     let mut time_in_sun = 0f32;
-    let mut shaded = false;
+    let mut shaded: bool = false;
     let mut plant_watered = false;
 
-    const DELAY_TIME: u32 = 100;//60 * 60 * 1000;
+    let mut time_since_shade = Instant::now();
+
+    const DELAY_TIME: u32 = /*100;*/60 * 60 * 1000;
 
     loop {
         //get water val
@@ -43,14 +72,15 @@ fn main() -> Result<(), Box<dyn Error>> {
         let sunlight = adc.read(&mut light_sensor)?;
         let sunlight_percent = 1f32 - (sunlight as f32 / 3038f32);
 
-        //println!("{:.2}", sunlight_percent);
-        //println!("{:.2}", wetness);
-
         //Make watering inference
-        let water_inf = water_model.predict(
+        /*let water_inf = water_model.predict(
             &vec![sunlight_percent, wetness, days_since / 10f32])[0];
-        if water_inf >= 0.9 {
+        */
+        if true {
             //Water plant
+            relay.set_low()?;
+            FreeRtos::delay_ms(500);
+            relay.set_high()?;
 
             plant_watered = true;
         } else {
@@ -62,8 +92,20 @@ fn main() -> Result<(), Box<dyn Error>> {
             &vec![sunlight_percent, wetness, time_in_sun]
         )[0];
 
-        if sun_inf > 0.7 {
-            //Move servo
+        if sun_inf > 0.7 && !shaded {
+
+            move_to(&mut driver, 30..200, min_limit, max_limit)?;
+            shaded = true;
+            time_since_shade = Instant::now();
+            
+        } else if shaded {
+
+            let total_sun_time = Instant::now().duration_since(time_since_shade);
+            if total_sun_time.as_millis() >= 300000 {
+                move_to(&mut driver, 200..30, min_limit, max_limit)?;
+                shaded = false;
+            }
+
         }
 
         //If we didn't do an operation, reset counter
@@ -76,3 +118,17 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 }
 
+
+fn map(x: u32, in_min: u32, in_max: u32, out_min: u32, out_max: u32) -> u32 {
+    (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+}
+
+fn move_to(driver:&mut LedcDriver, range: Range<usize>, min_limit: u32, max_limit: u32) -> Result<(), Box<dyn Error>> {
+    for angle in (30..=200).step_by(2) {
+        driver
+            .set_duty(map(angle, 0, 180, min_limit, max_limit))
+            .unwrap();
+        FreeRtos::delay_ms(25);
+    }
+    Ok(())
+}
